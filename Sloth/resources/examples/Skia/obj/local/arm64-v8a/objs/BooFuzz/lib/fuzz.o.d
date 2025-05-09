8088d012be7fbee0e91adfe8d0f084b4532ceffa
Cyberoiide <clement.bosle.dev@gmail.com>
2025-05-09 23:58:19 +0200

    Clean initial commit without sloth_content.zip

---
+++ Sloth/resources/examples/Skia/obj/local/arm64-v8a/objs/BooFuzz/lib/fuzz.o.d
@@ -0,0 +1,4 @@
+/examples/Skia/obj/local/arm64-v8a/objs/BooFuzz/lib/fuzz.o: \
+  /examples/Skia/jni/lib/fuzz.cpp /examples/Skia/jni/lib/fuzz.h
+
+/examples/Skia/jni/lib/fuzz.h:
---
+++ Sloth/resources/examples/Skia/obj/local/arm64-v8a/objs/boofuzz/boo.o.d
@@ -0,0 +1,4 @@
+/examples/Skia/obj/local/arm64-v8a/objs/boofuzz/boo.o: \
+  /examples/Skia/jni/boo.cpp /examples/Skia/jni/lib/fuzz.h
+
+/examples/Skia/jni/lib/fuzz.h:
